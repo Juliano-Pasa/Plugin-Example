158a2b82e44ef9f6b99249375e2503490bcec27e
Juliano Pasa <julianodemellopasa@gmail.com>
2025-02-15 13:04:59 -0300

    Game Tick counting

--- src/main/java/com/example/ExamplePlugin.java
+++ src/main/java/com/example/ExamplePlugin.java
@@ -2,15 +2,20 @@ package com.example;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
+import com.google.inject.spi.Message;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
+import java.text.MessageFormat;
 
 @Slf4j
 @PluginDescriptor(
@@ -24,16 +29,19 @@ public class ExamplePlugin extends Plugin
 	@Inject
 	private ExampleConfig config;
 
+	private int count;
+
 	@Override
 	protected void startUp() throws Exception
 	{
+		count = 0;
 		log.info("Example started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info(MessageFormat.format("Example stopped after running {0} game ticks!", count));
 	}
 
 	@Subscribe
@@ -44,6 +52,13 @@ public class ExamplePlugin extends Plugin
 			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
 		}
 	}
+
+	@Subscribe
+	public void onGameTick(GameTick gameTick)
+	{
+		count += 1;
+	}
+
 
 	@Provides
 	ExampleConfig provideConfig(ConfigManager configManager)
